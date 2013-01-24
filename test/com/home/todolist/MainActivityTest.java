8241fa40f0ac1d6808242134d960bdc2544e9aba
marekozw <marek.ozw@gmail.com>
2013-01-24 15:15:24 +0100

    Dodanie katalogu zrodlowego test/

---
+++ test/com/home/todolist/MainActivityTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.home.todolist;
+
+import junit.framework.TestCase;
+
+/**
+ * @author mape
+ *
+ */
+public class MainActivityTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
